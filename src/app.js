6d1caeb9e7924c09d59cf279282f1f5d6abe5b90
DevSatish2725 <mauryasatish146@gmail.com>
2026-05-22 21:11:25 +0530

    Added all users, delete, and patch

--- src/app.js
+++ src/app.js
@@ -16,6 +16,44 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+// Feed API - GET/feed - get all the users from the database
+app.get("/feed", async (req, res) => {
+  try {
+    const allUsers = await User.find({});
+    res.send(allUsers);
+  } catch (err) {
+    res.status(400).send("Error while fetching all users.");
+  }
+});
+
+// Delete a user from the database
+app.delete("/user", async (req, res) => {
+  try {
+    const userId = req.body.userId;
+    await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully.");
+  } catch (err) {
+    res.status(400).send("Getting error while deleting a user.", err.message);
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    await User.findByIdAndUpdate(userId, data);
+    res.send("User updated successfully.");
+  } catch (err) {
+    res.status(400).send("Error while updating an user.");
+  }
+});
+
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("Something went wrong.");
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("Database connected successfully.");
